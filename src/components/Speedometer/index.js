882cde457b7e12c9318324c055e72ef06205c223
Rashid <profRashid74@gmail.com>
2023-07-20 16:06:43 +0000

    commit message

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,12 @@
+import {Components} from 'react'
+
+class Counter extends Components {
+  render() {
+    return (
+      <div>
+        <h1>hi Rashid</h1>
+      </div>
+    )
+  }
+}
+export default Counter
